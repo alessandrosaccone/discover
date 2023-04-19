71ab0a915429bca1fd6a0356669a14135150fdfd
alessandrosaccone <alessandrosaccone96@gmail.com>
2023-04-19 17:52:40 +0200

    aggiorno

--- js/script_bacheca.js
+++ js/script_bacheca.js
@@ -6,14 +6,18 @@
 function button() {
   document.getElementById("form").style.display = "block";
   document.getElementById("view_area").style.display = "none";
+  document.getElementById("msg").style.display = "none";
 }
 
 function button_2() {
   document.getElementById("view_area").style.display = "block";
   document.getElementById("form").style.display = "none";
   document.getElementById("msg").style.display = "none";
-  $('#view').on('click', function() {
-    $('#view_area').load("bacheca_schede.php", {nome: 'Sara', cognome: 'Manzi'});
+  $("#view").on("click", function () {
+    $("#view_area").load("bacheca_schede.php", {
+      nome: "Sara",
+      cognome: "Manzi",
+    });
   });
 }
 
