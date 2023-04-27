648ce7bd5c655c86046e095bd76d5e46aadd6b21
Masergio1 <marioreda81@gmail.com>
2023-04-27 14:10:38 +0200

    benvenuto

--- login.php
+++ login.php
@@ -8,11 +8,13 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $query = "SELECT FROM utente_guida WHERE email = '$email' AND password = '$password'";
+    $query = "SELECT * FROM utente_guida WHERE email = '$email' AND password = '$password'";
     $result = pg_query($dbconn, $query);
+    $tuple = pg_fetch_array($result, null, PGSQL_ASSOC);
 
     if(pg_num_rows($result) > 0) {
         $_SESSION['username'] = $email;
+        $_SESSION['nome'] = $tuple["nome"];
         header("Location: home.html");
     } else {
         header("Location: login.html");
